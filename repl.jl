5ba0c4535dcfff90239a9b6c83951e7a5fb6761c
Enrico Lumetti <enrico.lumetti@gmail.com>
2021-02-23 20:55:06 +0100

    Don't import REPL inside nested module (rest is WIP but working)

--- repl.jl
+++ repl.jl
@@ -1,19 +1,12 @@
 import REPL
 
-function create_repl_module()
-    repl_module = Module()
-    Base.eval(repl_module, Meta.parse("import REPL"))
-    return repl_module
-end
-
-repl_module = create_repl_module()
+repl_module = Module()
 is_running = true
 while is_running
     input_fifo_path = ARGS[1]
     output_fifo_path = ARGS[2]
 
     open(input_fifo_path, "r") do input_fifo
-
         command = read(input_fifo, String)
         # input command format:
         # eval code
@@ -30,7 +23,6 @@ while is_running
         if cmd_end == nothing
             cmd_end = length(command)+1
         end
-
         cmd_name = command[1:cmd_end-1]
         if cmd_name == "eval"
            try 
@@ -44,22 +36,38 @@ while is_running
            to_complete = rstrip(command[cmd_end+1:end])
            expr = "REPL.REPLCompletions.completions(\"$(escape_string(to_complete))\", $(length(to_complete)))"
            println(expr)
-           completions_res = Base.eval(repl_module, Meta.parse(expr))
+           completions_res = REPL.REPLCompletions.completions(String(to_complete), length(to_complete), repl_module)
            completions = completions_res[1]
            completion_range = completions_res[2]
-           text_completions = String[]
+           kakoune_completions = String[]
            completion_success = completions_res[3] && !isempty(completion_range) && !isempty(completions)
            if completion_success
                for c in completions
-                   push!(text_completions, REPL.REPLCompletions.completion_text(c))
+                   completion_cmd = ""
+                   completion_unhandled = false
+                   if c isa REPL.REPLCompletions.BslashCompletion
+	                   completion_text = REPL.REPLCompletions.completion_text(c)
+                       completion_cmd = "echo \'a\'"
+	                   completion_menu = completion_text
+                   elseif c isa REPL.REPLCompletions.ModuleCompletion
+	                   completion_text = REPL.REPLCompletions.completion_text(c)
+                       completion_cmd = ""
+                       completion_menu = "$(c.mod)   {MenuInfo}$(c.mod)"
+                   else
+                       completion_unhandled = true
+                   end
+
+                   if !completion_unhandled
+	                   push!(kakoune_completions, "\"$completion_text|$completion_cmd|$completion_menu\"")
+                   end
                end
            end
-           
+           completion_success &= !isempty(kakoune_completions)
            open(output_fifo_path, "w") do fifo
                if completion_success
                    output = "$(completion_range.start):$(length(completion_range))"
                    output *= " "
-                   output *= join(["\"$c||$c\"" for c in text_completions], " ")
+                   output *= join(kakoune_completions, " ")
                    print(fifo, output)
                    println(output)
                end
